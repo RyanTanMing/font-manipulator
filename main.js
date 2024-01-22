69a3f017bf2becd395de0752c1351506c6eb5613
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2024-01-22 17:14:02 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
+nosex=0
+nosey=0
+rightwristx=0
+leftwristx=0
+difference=0
 function setup() {
-    canvas=createCanvas(500,500)
-    canvas.position(200,200)
+    video=createCapture(VIDEO)
+    video.size(550,500)
+    canvas=createCanvas(600,600)
+    canvas.position(700,150)
+    pn=ml5.poseNet(video,model_loaded)
+    pn.on('pose',got_poses)
+}
+function draw() {
+    background('cyan')
+    document.getElementById("square_side").innerHTML="Width and height of the text will be: "+difference+"px"
+    textSize(difference)
+    text("Ryan",nosex,nosey)
+}
+function model_loaded() {
+    console.log("posenet has been initialnised")
+}
+function got_poses(results) {
+    if (results.length>0) {
+        //console.log(results)
+        nosex=results[0].pose.nose.x
+        nosey=results[0].pose.nose.y
+        leftwristx=results[0].pose.leftWrist.x
+        rightwristx=results[0].pose.rightWrist.x
+        difference=Math.floor(leftwristx-rightwristx)
+        console.log(difference)
+    }
 }
